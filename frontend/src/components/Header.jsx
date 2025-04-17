c08538e751b34f39dcb8c2787a9b10cc32cc79d0
OriRoth22 <rori@post.bgu.ac.il>
2025-04-17 22:53:08 +0300

    edited smal things

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,18 +8,21 @@ const Header = () => {
         <header className="flex flex-col sm:flex-row justify-between items-center p-4">
             {/* Logo & Greeting */}
             <div className="flex flex-col items-center sm:items-start">
-                <img src={logo} alt="GiveIt Logo" className="w-32 h-auto mb-1" />
+                <img src={logo} alt="GiveIt Logo" className="w-40 h-auto mb-1" />
                 {user && (
-                    <div className="text-2xl text-gray-700 font-semibold mt-2 ml-2">
-                        Hello {user.user?.firstName || user.firstName || 'User'}! 👋
+                    <div className="flex flex-col">
+                        <div className="text-2xl text-gray-700 font-semibold mt-2 ml-2">
+                            Hello {user.user?.firstName || user.firstName || 'User'}! 👋
+                        </div>
+                        <div className="text-lg text-gray-500 ml-2">
+                            nice to see you again:)
+                        </div>
                     </div>
                 )}
             </div>
 
             {/* Centered Title */}
-            <h1 className="text-3xl font-bold text-center mt-4 sm:mt-0">
-                nice to see you again:)
-            </h1>
+
         </header>
     );
 };
