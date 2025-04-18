ed1f13564b4ee3702147edb1f76baf79493d6882
Carmela <carmelaroz@gmail.com>
2025-04-18 12:09:24 +0300

    removed logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header className="flex flex-col sm:flex-row justify-between items-center p-4">
             {/* Logo & Greeting */}
             <div className="flex flex-col items-center sm:items-start">
-                <img src={logo} alt="GiveIt Logo" className="w-40 h-auto mb-1" />
+                {/* <img src={logo} alt="GiveIt Logo" className="w-40 h-auto mb-1" /> */}
                 {user && (
                     <div className="flex flex-col">
                         <div className="text-2xl text-gray-700 font-semibold mt-2 ml-2">
